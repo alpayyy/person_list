b378a81e598fe06ccfff9f27115df00530c07a3b
alpayyy <115874416+alpayyy@users.noreply.github.com>
2023-07-30 12:06:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+let data; // Declare 'data' as a global variable
+
 document.addEventListener('DOMContentLoaded', function () {
   fetch('person-list.json')
     .then(response => response.json())
-    .then(data => {
+    .then(jsonData => {
+      data = jsonData.personList; // Assign 'personList' from JSON data to the global variable 'data'
       const personList = document.getElementById('person-list');
-      updatePersonList(personList, data.personList);
+      updatePersonList(personList, data);
     })
     .catch(error => {
       console.error('Veri çekilemedi:', error);
@@ -20,26 +23,35 @@ function updatePersonList(personList, data) {
     return fullName.includes(filterInput);
   });
 
-  filteredData.forEach(item => {
+  if (filteredData.length === 0) {
+    // Display error message
     const listItem = document.createElement('li');
-    listItem.classList.add('person');
-    listItem.innerHTML = `
-      <strong>${item.ad} ${item.soyad}</strong>
-      <br>
-      Telefon: ${item.telefon}
-      <br>
-      Email: ${item.email}
-      <button class="delete-btn" onclick="deletePerson(this)">Sil</button>
-    `;
+    listItem.textContent = 'Aradığınız kriterlere uygun kişi bulunamadı.';
     personList.appendChild(listItem);
-  });
+  } else {
+    filteredData.forEach(item => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('person');
+      listItem.innerHTML = `
+        <strong>${item.ad} ${item.soyad}</strong>
+        <br>
+        Telefon: ${item.telefon}
+        <br>
+        Email: ${item.email}
+        <button class="delete-btn" onclick="deletePerson(this)">Sil</button>
+      `;
+      personList.appendChild(listItem);
+    });
+  }
 }
+
 
 function deletePerson(button) {
   const listItem = button.parentElement;
   listItem.remove();
 }
+
 function filterList() {
   const personList = document.getElementById('person-list');
-  updatePersonList(personList, data);
+  updatePersonList(personList, data); 
 }
